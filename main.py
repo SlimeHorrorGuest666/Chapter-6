16663e338dc13a11bfab375733410e1e6faa4d74
Hikam Akbar Maulana <111330573+SlimeHorrorGuest666@users.noreply.github.com>
2022-08-31 17:33:35 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,49 @@
+print("Gondarwah")
+print("Chapter 6")
+print(
+    "T̶̯̓̆͗ḩ̷̛̮̙̩͖̘͎̞͉̳̙̦̥̈́̓̎̈́̀̄̀̌͊̍͂͑͘ę̶̟̲͍͎̬̥̣̌̽̃͆̈͂̊͌̂̀̏̎̿́͌͋̆̇̓͜ ̸̺͍̝̊̌̉̋̅̏̌͑̑̾̀͠L̵̨̡̛̥͔̗͍̜̮͕͍͔͕̝̟͚̟͛̌̌̐͗͗̂͗̇͗͛͊̏͘͝͝͠ͅŏ̸͈̦̗̞̼̈̀̀s̴̖͉̥̘̈́͂̎̀͝t̸̢͖̟̳̩̳̲͎̱̘̩͇̙̦̭͓̃͒̏͒̆͑̔͗͛͒̃́̔͆͒̾̕̚ ̸̨̧̦̤͚̱̗͍͎͈̻͉̘͉̘͇̗̒̎̄̍̈͗͐͑̆͜F̴̧̧̭͓̯͙̙̳͖͈̀̌̔̔̈́͝ŗ̶̛̳͐͛̉͛͊͐̂̎̊̔͛̾̀͛̇̒͆̎̈́͝i̸͎͊͗̐̾̑̈́͋̾̐͒̕e̸̢̡̧̩̰̖͕̳̞͔̲̣͂̈͋̆͠ͅn̵̡̥̺̫͎͔̗̩̝̥̥̝͙̾͊͌̽̄̿̃̐̂͒͋̐̀̄̾̄͌͛́̚͠ͅd̶̟̆͛̓"
+)  #The Devil Or Creature?
+print("Hey Diamond! What Are You Doing, Jony?")
+print("I'm Looking The-Man-With-Upside-Down-Head In The Old Photo")
+print("Jony & Rangga Looking This Man In The Old Photo Is Creepy"
+      )  #The-Man-With-Upside-Down-Head Is In Tragedy Broken Car
+print(
+    "Everyone Looking Again Huggy-Wuggy In The Same Photo But Different The Place"
+)
+print(
+    "They Are Found Again The VHS Tape About Who Is The-Man-With-Upside-Down-Head"
+)  #The Man With Upside Down Head Is Creature Humanoid Negative Can Kill You In The Tragedy Street
+print(
+    "They're Found Again Easter Egg About Episode From Cartoon Spongebob Squarepants, R̵̛̤͊̓̈Ȩ̴͈̰̞̠̝̭̦̠̿̊͑͋̕͜͝D̸̢͖̼̳̤̭̆͛͑ ̸̡̧̛͈͚̰̭̹̟͓̖̥̈̀̆̑̏̐̔́̚M̵̪͚͇̣͆̾̒̾̈́͛̊̈́̈́͛̾̐̀͘̕͝͝I̴̧̪̠̯̯̝͍͖̮͍̮͔͖͚̝̓̇̽̒̕̕͝ͅS̶̢̛̬̙̲̙͓̈́͑̑͋̎̔͌̍̏́̒̇͐̀̈́́ͅŢ̵̛̹̟͚͇͖͆̍̀̄̐̐ In Brains 4 But There Text And Picture Previously There Isn't Any"
+)
+print(
+    "Jony Trying To Delete Episode Not About Cartoon Spongebob Squarepants With Pulpen Deleter But Although Delete But There Text And Picture Always It"
+)
+print(
+    "Buttercam Is Perpetrator From All Of This To Written That About Episode R̸̨̜͈̝͙̰͓͈̻͉͈̦̖͇̠̍͠Ę̶̧̛͎̬̰̬̦͖͂̏͂͊̋̏͒̎͆̊̃̅͋́̾͜͜͠D̵̮̭̘̳̪̟͕͇͉͛́̐̓͗͊͐͘ ̷̧̡̨̞̫̠̖̖̫͓͚̤̼̂̔͊̃̊̀̒́͂̈͘ͅM̸̭̺̭̦̝̲̩̻̭̺͇͍͔͐͑̾͝͝͝I̸̢̧͚̠̱̱̠̳̻͂̓̓̏̒́̈́̿͊ͅͅŞ̴̗̦̩͔̂͗̓͋̇̀̏͘ͅT̴̨̠̦̟̩̫͓̱̗͓͎͚̣͍̀̆͗́͛̏͛͆͆̕͝  And Send That Two Photo Is In 12 Januari 2021"
+)
+print("Buttercam Wanna Say's This Letter In Brains 4 For Jony & Rangga")
+print(
+    "Hello Jony & Rangga, I'm Buttercam Want To Make Party At The Home And Coming Here To Enjoy With Your Friend And COME AT 10:00 AM NOW!"
+)
+print(
+    "They Are Coming Her House But They Are Looking Her House Very Horrible. It Turns Out, Her House Is Very Horrible Because The Halloween Is Coming"
+)
+print("Jony Want To Say's To The Buttercam Is Where Is My Friend")
+print(
+    "Buttercam Say's Your Friend In Your Secret Place And That Is So Big With Triangle Hole And Want To Suicide"
+)  #Meaning Adit Want To Suicide Himself In The Portal Made By Jony And Bill Cipher At 2017 Because He Is Annoyed About His Family Death By Rangga. Rangga Is A Killer International And Now To The Detective International
+print(
+    "Know That, They're Wait To Make Party With Their Neighbor And After Make That With Lost Gravity"
+)
+print(
+    "They Are Running To The Home And Looking Their Friend To Open That Portal And Want To Entry"
+)
+print(
+    "But Rangga Apologize his Friend About His Public Past To The Adit But Adit Say's That Is Fake Apology"
+)
+print(
+    "Moment He Is Entry That Portal, Suddenly The Portal Is Make An Explosion And Make United States And China Is Almost Broken And The Man Is Height 3m Coming To The Real life And Say's"
+)
+print("I'm Your Brother, Don't Scare I'm Ok!")
+print("My Brother")
